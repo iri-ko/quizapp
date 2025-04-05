dcb12faeb9bb338cde4434b5f88057485b4d0e98
Irene Kober <irene.kober@gmx.net>
2025-04-05 20:09:27 +0200

    added correct answer counter function at endscreen

--- script.js
+++ script.js
@@ -72,29 +72,37 @@ const questions = [
 
 let currentQuestion = 0;
 
+let rightQuestions = 0;
+
 function init() {
     document.getElementById("all-questions").innerHTML = questions.length;
-    document.getElementById("currentQuestionNumber").innerHTML = currentQuestion+1;
+    
     showQuestion();
+
 }
 
 function showQuestion() {
-let question = questions[currentQuestion];
+    let question = questions[currentQuestion];
 
-    if(currentQuestion >=questions.length) {
-        //TO DO SHOW END SCREEN
-        document.getElementById('endScreen').style= '';
-        document.getElementById('question-body').style= 'display: none;'
+    if (currentQuestion >= questions.length) {
+        
+        document.getElementById("endScreen").style = "";
+        document.getElementById("question-body").style = "display: none;";
+        document.getElementById('finalAnswerNumbers').innerHTML = questions.length;
+        document.getElementById("amount-right-questions").innerHTML = rightQuestions;
+        
 
     } else {
+        document.getElementById("question-text").innerHTML =
+            question["question"];
+
+        document.getElementById("answer_1").innerHTML = question["answer_1"];
+        document.getElementById("answer_2").innerHTML = question["answer_2"];
+        document.getElementById("answer_3").innerHTML = question["answer_3"];
+        document.getElementById("answer_4").innerHTML = question["answer_4"];
+    }
+
     
-
-    document.getElementById("question-text").innerHTML = question["question"];
-
-    document.getElementById("answer_1").innerHTML = question["answer_1"];
-    document.getElementById("answer_2").innerHTML = question["answer_2"];
-    document.getElementById("answer_3").innerHTML = question["answer_3"];
-    document.getElementById("answer_4").innerHTML = question["answer_4"];}
 }
 
 function answer(selection) {
@@ -107,6 +115,7 @@ function answer(selection) {
         document
             .getElementById(selection)
             .parentNode.classList.add("bg-success");
+            rightQuestions++;
     } else {
         document
             .getElementById(selection)
@@ -119,7 +128,7 @@ function answer(selection) {
     //document.getElementById("btn-next-q").disabled = false;
 }
 
-function nextQuestion(){
+function nextQuestion() {
     currentQuestion++;
     showQuestion();
 
@@ -127,17 +136,33 @@ function nextQuestion(){
 
     resetAnswerButtons();
 
-    document.getElementById("currentQuestionNumber").innerHTML = currentQuestion+1;
+    document.getElementById("currentQuestionNumber").innerHTML =
+        currentQuestion + 1;
 }
 
-function resetAnswerButtons(){
-    document.getElementById('answer_1').parentNode.classList.remove('bg-danger');
-    document.getElementById('answer_1').parentNode.classList.remove('bg-success');
-    document.getElementById('answer_2').parentNode.classList.remove('bg-danger');
-    document.getElementById('answer_2').parentNode.classList.remove('bg-success');
-    document.getElementById('answer_3').parentNode.classList.remove('bg-danger');
-    document.getElementById('answer_3').parentNode.classList.remove('bg-success');
-    document.getElementById('answer_4').parentNode.classList.remove('bg-danger');
-    document.getElementById('answer_4').parentNode.classList.remove('bg-success');
+function resetAnswerButtons() {
+    document
+        .getElementById("answer_1")
+        .parentNode.classList.remove("bg-danger");
+    document
+        .getElementById("answer_1")
+        .parentNode.classList.remove("bg-success");
+    document
+        .getElementById("answer_2")
+        .parentNode.classList.remove("bg-danger");
+    document
+        .getElementById("answer_2")
+        .parentNode.classList.remove("bg-success");
+    document
+        .getElementById("answer_3")
+        .parentNode.classList.remove("bg-danger");
+    document
+        .getElementById("answer_3")
+        .parentNode.classList.remove("bg-success");
+    document
+        .getElementById("answer_4")
+        .parentNode.classList.remove("bg-danger");
+    document
+        .getElementById("answer_4")
+        .parentNode.classList.remove("bg-success");
 }
-
